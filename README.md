8c49f3c85978ffe91305f0fc6d0a42cf6b5d4ec5
Amado Gutierres <amadocardenas0@gmail.com>
2021-07-30 20:58:10 -0600

    forecast and crrnt weather

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Weather-Dashboard   
+
+## Description
+Weather Dashoboard is a app that allows you to get the current and upcoming weather conditions for any city, it also saves your past searches for quick access to a previoous city. For this task i used the openweather api to get the weather, jquery for a dynamic page.
+
+## Preview
+![Page Preview](./assets/images/weather-dashboard-image2.png)
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Credits](#credits)
+* [Questions](#questions)
+
+## Installation
+
+The page is deployed in the link below:
+https://amado-bot.github.io/Weather-Dashboard/
+
+
+## Credits
+
+Amado-bot
+
+## Questions
+
+https://github.com/Amado-bot
+
+amadocardenas0@gmail.com
+
--- js/index.js
+++ js/index.js
@@ -1,24 +1,103 @@
+// capture input as city
 let city = $("#searchValue").val();
 
+// save api key as key
 const key = "27be5eaf78baf6d8d8bf43cfd9dbb859"
 
-$("#searchValue").keypress(function(event){
+$("#searchValue").keypress(function (event) {
     if (event.keycode === 13) {
         event.preventDefault();
         $("#searchBtn").click();
     }
 });
 
-$("#searchBtn").on("click", function(){
-    $("#fiveDay").addClass("show");
-    $("#searchValue").val("");
+$("#searchBtn").on("click", function () {
+$("#fiveDay").addClass("show");
+city = $("#searchValue").val();
 
-    const apiQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+$("#searchValue").val("");
 
-    $.ajax({
-        url: apiQuery,
-        method: "GET"
-    })
-    
-    })
+const apiQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + city + key;
+
+$.ajax({
+url: apiQuery,
+method: "GET"
+.then(function (weather) {
+
+var nowMoment = moment();
+
+var displayMoment = $("<h3>");
+$("#city-name").empty();
+$("#city-name").append(
+    displayMoment.text("(" + nowMoment.format("M/D/YYYY") + ")")
+);
+
+var cityName = $("<h3>").text(weather.name);
+$("#city-name").prepend(cityName);
+
+let listItem = $("<li>").addClass("list-group-item").text(city);
+$(".list").append(listItem);
+
+var weatherIcon = $("<img>");
+weatherIcon.attr(
+    "src",
+    "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
+);
+$("#current-icon").empty();
+$("#current-icon").append(weatherIcon);
+
+function getCurrentConditions(response) {
+
+    let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    tempF = Math.floor(tempF);
+
+    $('#currentCity').empty();
+
+    const card = $("<div>").addClass("card");
+    const cardBody = $("<div>").addClass("card-body");
+    const city = $("<h4>").addClass("card-title").text(response.name);
+    const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+    const temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+    const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+    const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+
+    city.append(cityDate, image)
+    cardBody.append(city, temperature, humidity, wind);
+    card.append(cardBody);
+    $("#currentCity").append(card)
+
+}
+
+$.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
+    method: "GET"
+}).then(function (response) {
+
+        $('#forecast').empty();
+
+        getCurrentConditions(response);
+        getCurrentForecast(response);
+        makeList();
+
+        let results = response.list;
+
+        let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+        let tempF = Math.floor(temp);
+
+        const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+        const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+        const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+        const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+        const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+
+        const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+
+        cardBody.append(cityDate, image, temperature, humidity);
+        card.append(cardBody);
+        $("#forecast").append(card);
+
+    }
+}
 });
+
+}
